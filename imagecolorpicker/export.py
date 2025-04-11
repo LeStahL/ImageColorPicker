9907255c9350acb1208b37e906230e165d3d3335
Alexander Kraus <a.kraus@glp.de>
2025-04-11 17:54:09 +0200

    Added HLSL exports.

--- imagecolorpicker/export.py
+++ imagecolorpicker/export.py
@@ -115,7 +115,72 @@ class Export:
                     ))
                     return f'const int gradient_count = {len(gradientList)};\nconst int all_cmap_coefficient_count = {len(cmaps)};\nconst int offsets_per_cmap[] = int[](\n    {offsetSlide}\n);\nconst vec3 all_cmap_coefficients[] = vec3[](\n    {coefficientSlide}\n);\nvec3 cmap(float t, int index) {{\n    vec3 a = all_cmap_coefficients[offsets_per_cmap[index + 1] - 1];\n    for(int i = offsets_per_cmap[index + 1] - 2; i >= offsets_per_cmap[index]; --i) {{\n        a = all_cmap_coefficients[i] + t * a;\n    }}\n    return a;\n}}'
         elif language == Language.HLSL:
-            pass
+            if representation == Representation.ColorMap:
+                cmap: list[vec3] = selectedGradient.coefficients
+                if selectedGradient._model == FitModel.HornerPolynomial:
+                    openStack: str = '\n        +t*('.join(map(
+                        lambda color: f'float3({color.x:.2f}, {color.y:.2f}, {color.z:.2f})',
+                        cmap,
+                    ))
+                    closeStack: str = ')' * (len(cmap) - 1)
+                    return f'float3 cmap_{Export.MakeIdentifier(selectedGradient._name)}(float t) {{\n    return {openStack}\n    {closeStack};\n}}\n'
+                elif selectedGradient._model == FitModel.Trigonometric:
+                    return f'float3 cmap_{Export.MakeIdentifier(selectedGradient._name)}(float t) {{\n    return float3({cmap[0].x:.4f}, {cmap[0].y:.4f}, {cmap[0].z:.4f}) + float3({cmap[1].x:.4f}, {cmap[1].y:.4f}, {cmap[1].z:.4f}) * cos(2. * pi * (float3({cmap[2].x:.4f}, {cmap[2].y:.4f}, {cmap[2].z:.4f}) * t + float3({cmap[3].x:.4f}, {cmap[3].y:.4f}, {cmap[3].z:.4f})));\n}}\n'
+            elif representation == Representation.Color3:
+                return f'float3({selectedColor.x:.2f}, {selectedColor.y:.2f}, {selectedColor.z:.2f})'
+            elif representation == Representation.Color4:
+                return f'float4({selectedColor.x:.2f}, {selectedColor.y:.2f}, {selectedColor.z:.2f}, 1)'
+            elif representation == Representation.NearestWeight:
+                return f'{selectedGradient.nearestWeightInColorMap(selectedColor):.2f}'
+            elif representation == Representation.Colors:
+                colorSlide = ',\n    '.join(list(map(
+                    lambda color: f'float3({color.x:.2f}, {color.y:.2f}, {color.z:.2f})',
+                    selectedGradient._colors,
+                )))
+                name: str = Export.MakeIdentifier(selectedGradient._name)
+                return f'const int {name}_color_count = {selectedGradient.colorCount};\nconst float3 {name}_colors[] = {{\n    {colorSlide}\n}};\n'
+            elif representation == Representation.Weights:
+                weightSlide = ',\n    '.join(list(map(
+                    lambda weight: f'{weight:.2f}',
+                    selectedGradient._weights,
+                )))
+                name: str = Export.MakeIdentifier(selectedGradient._name)
+                return f'const int {name}_weight_count = {len(selectedGradient._weights)};\nconst float {name}_weights[] = {{\n    {weightSlide}\n}};\n'
+            elif representation == Representation.ColorMaps:
+                cmaps: list[list[vec3]] = reduce(
+                    lambda accumulator, addition: accumulator + addition,
+                    map(
+                        lambda gradient: gradient.coefficients,
+                        gradientList,
+                    ),
+                )
+                coefficientSlide = ',\n    '.join(list(map(
+                    lambda color: f'float3({color.x:.2f}, {color.y:.2f}, {color.z:.2f})',
+                    cmaps,
+                )))
+                if Export.AllCMapsSameWidthSameModel(gradientList):
+                    # Now we can use modulo, integer division etc.
+                    cmap = list(map(
+                        lambda index: f'all_cmap_coefficients[index * single_gradient_size + {index}]',
+                        range(gradientList[0]._degree),
+                    ))
+                    result = f'const int gradient_count = {len(gradientList)};\nconst int single_gradient_size = {gradientList[0]._degree};\nconst float3 all_cmap_coefficients[] = {{\n    {coefficientSlide}\n}};\n'
+                    if selectedGradient._model == FitModel.HornerPolynomial:
+                        return result + f'float3 cmap(float t, int index) {{\n    float3 a = all_cmap_coefficients[(index + 1) * single_gradient_size - 1];\n    for(int i = single_gradient_size - 2; i >= 0; --i)\n        a = all_cmap_coefficients[index * single_gradient_size + i] + t * a;\n    return a;\n}}\n'
+                    elif selectedGradient._model == FitModel.Trigonometric:
+                        return result + f'float3 cmap(float t, int index) {{\n    return float3({cmap[0].x:.4f}, {cmap[0].y:.4f}, {cmap[0].z:.4f}) + float3({cmap[1].x:.4f}, {cmap[1].y:.4f}, {cmap[1].z:.4f}) * cos(2. * pi * (float3({cmap[2].x:.4f}, {cmap[2].y:.4f}, {cmap[2].z:.4f}) * t + float3({cmap[3].x:.4f}, {cmap[3].y:.4f}, {cmap[3].z:.4f})));\n}}\n'
+                else: 
+                    cmap_offsets = []
+                    offset = 0
+                    for gradient in gradientList:
+                        cmap_offsets.append(offset)
+                        offset += len(gradient.coefficients)
+                    cmap_offsets.append(len(cmaps))
+                    offsetSlide = ',\n    '.join(map(
+                        str,
+                        cmap_offsets,
+                    ))
+                    return f'const int gradient_count = {len(gradientList)};\nconst int all_cmap_coefficient_count = {len(cmaps)};\nconst int offsets_per_cmap[] = {{\n    {offsetSlide}\n}};\nconst float3 all_cmap_coefficients[] = {{\n    {coefficientSlide}\n}};\nfloat3 cmap(float t, int index) {{\n    float3 a = all_cmap_coefficients[offsets_per_cmap[index + 1] - 1];\n    for(int i = offsets_per_cmap[index + 1] - 2; i >= offsets_per_cmap[index]; --i) {{\n        a = all_cmap_coefficients[i] + t * a;\n    }}\n    return a;\n}}'
         elif language == Language.CSS:
             if representation == Representation.ColorMap:
                 colorStops = list(map(
